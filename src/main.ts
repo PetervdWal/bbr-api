423f60ce4de22aff75372933a9149aa7371cb361
Peter van der Wal <peterzakelijk@live.nl>
2023-10-17 15:50:12 +0200

    client proofing

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { BBRClient } from './clients';
+import { LeaderBoard } from './types';
 
 const client = new BBRClient();
 
-
-
-client.getServers().then((response) => {
-    if(response){
-        console.log('Counting servers:', response.length)
-        response.forEach((server) => {
-            console.log(server.Name);
-        })
-    }
+client.getLeaderBoard().then((response) => {
+  if (response) {
+    console.log('success');
+    console.log(response);
+  }
 });
-
-
